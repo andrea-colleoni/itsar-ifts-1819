c3b38ebfa5c0e895ede990e779e416003db839bd
laracorona <laracorona@172.20.10.2>
2018-11-13 12:14:14 +0100

    lara

---
+++ Lezione1/src/lezione01/Matematica.java
@@ -0,0 +1,12 @@
+package lezione01;
+
+public class Matematica {
+
+	public static void main(String[] args) {
+		int raggio = 5;
+		
+		System.out.println("L'area del cerchio è " + (Math.PI * Math.pow(raggio, 2)));
+		
+	}
+
+}
