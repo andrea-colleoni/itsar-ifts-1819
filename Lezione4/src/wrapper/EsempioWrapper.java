7d19dad2617ea05ca489591521493e6a1b5787f5
Andrea Colleoni <andrea@colleoni.info>
2018-11-15 14:23:49 +0100

    wrapper, boxing/unboxing

---
+++ Lezione4/src/wrapper/EsempioWrapper.java
@@ -0,0 +1,20 @@
+package wrapper;
+
+public class EsempioWrapper {
+
+	public static void main(String[] args) {
+		int a = 10;
+		Integer b = 20;
+		
+		char x = 'x';
+		Character y = 'y';
+		
+		double d = 10.5;
+		Double e = 10.8;
+		
+		String s = "ciao";
+		
+		System.out.println(a + b);
+	}
+
+}
